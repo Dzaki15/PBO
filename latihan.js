375b7393abe76bc1247c2cb27c6e9e0eabec501f
Tegar Dzaki Hakim <tegardzakihakim333@gmail.com>
2024-09-15 19:08:52 +0700

    Delete latihan.js

--- latihan.js
+++
@@ -1,9 +0,0 @@
-const tulis = document.getElementById('latihan')
-tulis.innerHTML = "ini adalah pertama saya melalui DOM"
-
-const h1 = document.getElementsByTagName("h1")
-const tulish1 = document.getElementById("h1")
-tulish1.innerHTML = " "
-for(let i=0; i<10; i++){
-    tulis,innerHTML += h1[0].innerHTML + "<br>"
-}
